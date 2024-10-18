7a5b0587ca1316d7f41e1f9c1539587b342e1ade
normie-6923 <anurag200610@gmail.com>
2024-10-18 09:45:02 +0530

    updated fs

--- fs_module.js
+++ fs_module.js
@@ -1,18 +1,18 @@
-const fs = require("fs")
+// const fs = require("fs")
 
-// const first = fs.readFileSync("./textfile/first.txt","utf8")
+// const first = fs.readFile("./first.txt","utf8")
 // console.log(first)
-// const second = fs.readFileSync("./textfile/second.txt","utf8")
+// const second = fs.readFile("./second.txt","utf8")
 // console.log(second)
 
-// fs.writeFileSync("./textfile/first.txt","this is the next text that would be writtne inside the first text file");
+// fs.writeFile("./first.txt","this is the next text that would be writtne inside the first text file.");
 // console.log("first file updated successfully!")
-// fs.appendFileSync("./textfile/second.txt","this is the next text that would be added inside the second text file");
+// fs.writeFile("./second.txt","this is the next text that would be added inside the second text file.");
 // console.log("second file updated successfully!")
 
-// const firstupdated = fs.readFileSync("./textfile/first.txt","utf8")
+// const firstupdated = fs.readFile("./first.txt","utf8")
 // console.log(firstupdated)
-// const secondupdated = fs.readFileSync("./textfile/second.txt","utf8")
+// const secondupdated = fs.readFile("./second.txt","utf8")
 // console.log(secondupdated)
 
 
@@ -23,3 +23,26 @@ const fs = require("fs")
 // fs.unlink("./textfile/first.txt",{recursive:true,force:true},(err)=>{
 //     console.log("removed file")
 // })
+
+
+//using util.promisify
+
+const fs = require("fs")
+const util = require("util")
+
+const readfilep = util.promisify(fs.readFile);
+const writefilep = util.promisify(fs.writeFile);
+
+const start = async()=>{
+    try{
+const first = await readfilep("first.txt","utf8")
+const second = await readfilep("second.txt","utf8")
+await writefilep("first.txt","this is changed now!")
+console.log(first+"\n",second)
+console.log("this ends here")
+    }
+    catch(error){
+console.log(error)
+    }
+}
+start()
